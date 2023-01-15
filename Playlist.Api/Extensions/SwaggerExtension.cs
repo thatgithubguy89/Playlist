8c6b24bcda4d34800fd412ff8b2d857bf5fa3bd2
Jory Adair <jorytristian@aol.com>
2023-01-15 05:43:44 -0500

    Add authorization to swagger

---
+++ Playlist.Api/Extensions/SwaggerExtension.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Playlist.Api.Extensions
+{
+    public static class SwaggerExtension
+    {
+        public static IServiceCollection AddSwaggerExtension(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Scheme = "Bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
